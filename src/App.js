ea957f8d3a271803488e7b10f9a7c45ac43925f7
AshishAcharya083 <imashish083@gmail.com>
2021-01-23 11:17:42 +0545

    tailwind css setup completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import 'tailwindcss/tailwind.css';
-import NavBar from './component.jsx/navBar';
+import './assets/main.css'
 
+
+import NavBar from './component.jsx/navBar';
+import Body from './component.jsx/body';
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Body/>
     </div>
   );
 }
--- src/component.jsx/body.jsx
+++ src/component.jsx/body.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-export default function MyBody() {
-  return <main>
+export default function Body() {
+  return (
+    <main>
       <div>
-          <a href>Login</a>
+        <a href>Login</a>
+        <a href>Sign UP</a>
       </div>
-  </main>
+
+      <header>
+        <h2>Recipes</h2>
+        <h3>In Nepali language</h3>
+      </header>
+      <div>
+        <h4>Popular recipes</h4>
+        <div>{/* Cards will go here */}
+            <div>
+                <img src="" alt="Food"/>
+                <div>
+                    <span>Steam MOMO</span>
+                    <span>मोमो हो यार</span>
+                </div>
+            </div>
+        
+        </div>
+      </div>
+    </main>
+  );
 }
